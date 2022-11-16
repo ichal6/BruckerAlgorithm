c44b5373f65ce469a27092abbcba0b4f82a8ff30
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-11-16 17:43:57 +0100

    Implement skeleton of graph

--- src/main/java/controller/Graph.java
+++ src/main/java/controller/Graph.java
@@ -8,18 +8,53 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
+import model.Task;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Graph implements Initializable {
     @FXML
     public Canvas canvas;
+    private Map<String, Task> tasksMap;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) { }
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.tasksMap = LoadData.tasksMap;
+        drawGraph();
+    }
+
+    private void drawGraph(){
+        GraphicsContext gc = this.canvas.getGraphicsContext2D();
+        Task currentTask = this.tasksMap.get("root");
+        List<Task> remainTasks = new ArrayList<>();
+        List<Task> childrenTasks = new ArrayList<>(currentTask.getPreviousTasks());
+        double x = 0, y = 0;
+        int siblingCount = 0;
+
+        gc.strokeOval(x, y,10,10); //draw root
+        while(!childrenTasks.isEmpty() || !remainTasks.isEmpty()){
+            if(remainTasks.isEmpty()){
+                y+=20;
+                x=0;
+                remainTasks.addAll(childrenTasks);
+                childrenTasks.clear();
+                siblingCount = currentTask.getPreviousTasks().size();
+            } else{
+                currentTask = remainTasks.remove(0);
+                childrenTasks.addAll(currentTask.getPreviousTasks());
+                x+= 20;
+                gc.strokeOval(x, y,10,10);
+            }
+        }
+
+    }
 
     public void returnToMainMenu(ActionEvent event){
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
